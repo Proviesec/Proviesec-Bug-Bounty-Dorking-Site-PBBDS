a466de1bd4bb1f8c908a3b9d5cc55fb9aab4bbde
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-08-29 12:44:54 +0200

    create main.js and add makelist function

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// import the array which acts as a data source for the list
+import { reconlist } from './reconlist.js'
+function makeList() {
+    // Make a container element for the list
+    listContainer = document.createElement('div'),
+    // Make the list
+    listElement = document.createElement('ul'),
+    // Set up a loop that goes through the items in listItems one at a time
+    numberOfListItems = reconlist.length, listItem, i;
+
+    document.getElementsById('reconlist').appendChild(listContainer);
+    listContainer.appendChild(listElement);
+  
+    for (i = 0; i < numberOfListItems; ++i) {
+        // create an item for each one
+        listItem = document.createElement('li');
+        // Add the item text
+        listItem.innerHTML = reconlist[i];
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+    }
+}
+
+makeList();
