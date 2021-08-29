6f81cfc5c18f0e97617d550e9b624f4bece2d64b
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-08-29 17:41:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'reconlist.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+}
 // import the array which acts as a data source for the list
 function makeList() {
+    loadJSON(function(response) {
+      // Parse JSON string into object
+        var reconlist = JSON.parse(response);
+     });
     // Set up a loop that goes through the items in listItems one at a time
     numberOfListItems = reconlist.length;
 
