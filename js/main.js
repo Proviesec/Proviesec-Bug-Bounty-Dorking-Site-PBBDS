50e5902dff90370df99698aa4764a6ad52d7836c
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-08-29 14:15:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function makeList() {
         // create an item for each one
         const listItem = document.createElement('li');
         // Add the item text
-        listItem.innerHTML = "<a href=''>Somelink</a>"+" "+reconlist[i];
+        listItem.innerHTML = "<a href='"+reconlist[i]['link']+"'>"+reconlist[i]['title']+"</a>";
         // Add listItem to the listElement
         listElement.appendChild(listItem);
     }
