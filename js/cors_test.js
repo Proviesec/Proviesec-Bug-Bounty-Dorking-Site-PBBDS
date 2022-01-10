8c4adbfaaff1dcaa681a8dd3e104a93b2941f63e
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-01-10 11:53:39 +0100

    Update cors_test.js

--- js/cors_test.js
+++ js/cors_test.js
@@ -1,9 +1,13 @@
 function cors_test(test_url) {
-    $.ajax ({
-        url: test_url,
-        success: function(data) 
-        {
-            console.log(data);
-        }
-    });
+    if (test_url) {
+        $.ajax ({
+            url: test_url,
+            success: function(data) 
+            {
+                console.log(data);
+            }
+        });
+    } else {
+        console.log("No URL Data");
+    }
 }
