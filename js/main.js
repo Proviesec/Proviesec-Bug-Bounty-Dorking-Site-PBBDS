a979e67490ad0dca01559b97d7331e1fe02d69ee
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-06-05 18:37:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -224,6 +224,7 @@ function findGetParameter(parameterName) {
           tmp = item.split("=");
           if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         });
+	
     return result;
 }
 
